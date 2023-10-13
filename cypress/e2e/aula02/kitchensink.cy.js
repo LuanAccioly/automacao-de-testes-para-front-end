2976c8d7d4868f1d4c099d0a6f93fcb201fd9b83
sidneycnogueira <69484633+sidneycnogueira@users.noreply.github.com>
2023-10-13 15:00:47 -0300

    formatando

--- cypress/e2e/aula02/kitchensink.cy.js
+++ cypress/e2e/aula02/kitchensink.cy.js
@@ -7,7 +7,6 @@ describe('Querying', () => {
   });
 
   it('get - busca botao', () => {
-  
     //busca pelo id
     cy.get('#query-btn').should('contain', 'Button')
     
@@ -21,33 +20,25 @@ describe('Querying', () => {
     //cy.get('#querying .row').should('have.have.length',5)
   });
   
-  
   it('get - busca elemento pelo test-id', () => {
-    
     cy.get('[data-test-id="test-example"]').should('have.class', 'example')
-    
   });
   
   it('contains - busca pelo conteúdo', () => {
-    
     //busca elemento da classe .query-list e procura na sua árvore 
     //elemento que contém bananas
     //verifica se a classe do elemento é igual a third
     cy.get('.query-list')
     .contains('bananas').should('have.class', 'third')
-  
     // busca utilizando expressão regular (regexp)
     cy.get('.query-list')
       .contains(/^b\w+/).should('have.class', 'third')
-  
     cy.get('#querying')
       .contains('ul', 'oranges')
       .should('have.class', 'query-list')
-    
-  
   });
   
-  it.only('within - busca dentro de um elemento', () => {
+  it('within - busca dentro de um elemento', () => {
     cy.get('#inputName').type('Meu nome')
     cy.get('.query-form').within(() => {
       cy.get('input:first').type('fake@gmail.com')
--- cypress/e2e/aula02/nao-recomendado.cy.js
+++ cypress/e2e/aula02/nao-recomendado.cy.js
@@ -2,17 +2,14 @@
 
 it('t1 - ok', () => {
     cy.visit('https://example.cypress.io/commands/actions')
-
     cy.get('.action-email').type('email')
     cy.get('#password1').type('abc')
 });
 
 it.only('t1 - nok', () => {
     cy.visit('https://example.cypress.io/commands/actions')
-
     const email = cy.get('.action-email')
     const pwd = cy.get('#password1')
-
     email.type('email')
     pwd.type('abc')
 });
