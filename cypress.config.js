0ee8d49e5e7cfdffb3524b568f09e9b5fb4f045d
sidneycnogueira <69484633+sidneycnogueira@users.noreply.github.com>
2023-10-22 11:23:49 -0300

    alterando as configurações

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  chromeWebSecurity: false,
+  chromeWebSecurity: false, // para acessar conteúdo de um iFrame
+  video : true, // para gravar vídeos ao rodar npx cypress run
   e2e: {
-    experimentalStudio: true,
+    experimentalStudio: true, //para habilitar o Selector Playground
     baseUrl : 'https://ecommerce-playground.lambdatest.io'
   },
 });
