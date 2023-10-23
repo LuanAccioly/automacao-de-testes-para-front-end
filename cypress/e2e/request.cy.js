9d14b68d73ed9758f42abda9899e5ab05204f251
Luan Accioly <acciolyluan@gmail.com>
2023-10-23 01:13:47 -0300

    adicionando comentários ao teste de request

--- cypress/e2e/request.cy.js
+++ cypress/e2e/request.cy.js
@@ -1,16 +1,24 @@
 it("testando request", () => {
   // Teste de GET
   cy.visit("https://example.cypress.io/commands/network-requests");
+
+  // Click no botão para fazer a requisição
   cy.get(".network-btn").click();
+
+  // Passamos a URL da requisição
   cy.request("https://jsonplaceholder.cypress.io/comments/1").then(
     (response) => {
+      // Todo código aqui dentro só será executado após a resposta do request
+
+      // Verificamos se o campo de resultado do site contém o conteúdo recebido pelo request
       cy.get(".network-comment").should("contain", response.body.body);
     }
   );
 
   // Teste de POST
   cy.get(".network-post").click();
-  cy.request("https://jsonplaceholder.cypress.io/comments").then((response) => {
+  cy.request("https://jsonplaceholder.cypress.io/comments").then(() => {
+    // Verificamos se o campo de resultado do site contém o conteúdo recebido pelo request
     cy.get(".network-post-comment").should("contain", "POST successful!");
   });
 });
